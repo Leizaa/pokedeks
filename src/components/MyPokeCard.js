463cf450c681b3bd7deebd9aacbc7b8c5aea865e
Leizaa <yudiajalah2@gmail.com>
2021-03-26 20:55:25 +0700

    update my poke card

--- src/components/MyPokeCard.js
+++ src/components/MyPokeCard.js
@@ -1,22 +1,6 @@
 import React from "react";
 import { Button, Card, Image, Label } from "semantic-ui-react";
 
-// const releasePoke = (self) => {
-//   var pokeList = JSON.parse(localStorage.getItem("catched"));
-//   console.log("before", pokeList);
-//   for (let i = 0; i < pokeList.length; i++) {
-//     if (
-//       pokeList[i].nickname === self.nickname &&
-//       pokeList[i].name === self.name
-//     ) {
-//       pokeList.splice(i, i + 1);
-//       break;
-//     }
-//   }
-//   console.log("after", pokeList);
-//   localStorage.setItem("catched", JSON.stringify(pokeList));
-// };
-
 function MyPokeCard({ poke, handleOnClick }) {
   const getTypes = (types) => {
     var pokeTypes = [];
